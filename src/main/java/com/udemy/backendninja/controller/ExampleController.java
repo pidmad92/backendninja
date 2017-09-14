388d8064b7c3ffb73cd37534b053a670d8a2743b
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-09-14 11:48:51 -0500

    Dentro de ExampleController se creó el una Funcion lista getPeople() que contenia una lista people con objetos person que devolvía la lista people; esto se puso como atributo de retorno, es decir devuelve un atributo de nombre people, cuyo valor es getPeople()

--- src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller // Anotación diseñada para generar las vistas, ésta hereda de @Component
 @RequestMapping("/example") // Anotación diseñada para generar una URL en la que se generará la vista
 public class ExampleController {
@@ -17,7 +20,7 @@ public class ExampleController {
     // Primera forma - Para redireccionar las plantillas
     @RequestMapping(value = "/exampleString", method = RequestMethod.GET) // Anotación usada porque se trata de una petición Get y se le indica la ruta.
     public String exampleString(Model model){
-        model.addAttribute("person",new Person("Pierre",25)); // En el modelAtribute se ingresa el nombre de la variable seguido del valor
+        model.addAttribute("people", getPeople()); // En el modelAtribute se ingresa el nombre de la variable seguido del valor
         return EXAMPLE_VIEW; // Se mostrará la plantilla example.html
     }
 
@@ -25,8 +28,17 @@ public class ExampleController {
     @GetMapping("/exampleMAV") // Anotación usada porque se trata de una petición Get y se le indica la ruta.
     public ModelAndView exampleMAV(){
         ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
-        mav.addObject("person",new Person("Pierre",25));
+        mav.addObject("people", getPeople());
         return mav; // Se mostrará la vista example.html
+    }
+
+    private List<Person> getPeople(){
+        List<Person> peoble = new ArrayList<>();
+        peoble.add(new Person("Pierre",25));
+        peoble.add(new Person("Milagros",26));
+        peoble.add(new Person("Jose",20));
+        peoble.add(new Person("Santiago",18));
+        return peoble;
     }
 
 }
