2be10d9506b8fe7b1d5c7e48625278ab9008e89b
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-09-14 11:16:24 -0500

    En la vista example.html se borro la plabra <World> y fue remplazada por un span cuyo atributo el de Thymeleaf th:text= y cuyo valor fue una salida a imprimir un nombre ; para lo cual se altero el controlador ExampleController en el que se muestra dos formas de enviar el atributo nombre (Model model.addAtribute(nombredevariable,valor) y ModelAndView mav.addObject(=) donde mav carga con la vista y se devuelve a si mismo)

--- src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package com.udemy.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,18 +11,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/example") // Anotación diseñada para generar una URL en la que se generará la vista
 public class ExampleController {
 
-    public static final String EXAMPLE_VIEW = "example";
+    private static final String EXAMPLE_VIEW = "example";
 
     // Primera forma - Para redireccionar las plantillas
     @RequestMapping(value = "/exampleString", method = RequestMethod.GET) // Anotación usada porque se trata de una petición Get y se le indica la ruta.
-    public String exampleString(){
+    public String exampleString(Model model){
+        model.addAttribute("name","Pierre"); // En el modelAtribute se ingresa el nombre de la variable seguido del valor
         return EXAMPLE_VIEW; // Se mostrará la plantilla example.html
     }
 
     // Segunda Forma - Para incertar datos en la plantillas
     @GetMapping("/exampleMAV") // Anotación usada porque se trata de una petición Get y se le indica la ruta.
     public ModelAndView exampleMAV(){
-        return new ModelAndView(EXAMPLE_VIEW); // Se mostrará la vista example.html
+        ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+        mav.addObject("name","Pierre");
+        return mav; // Se mostrará la vista example.html
     }
 
 }
